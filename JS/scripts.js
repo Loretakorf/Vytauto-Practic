6279045ab709614bd6ea302ccf2e2a9d0da91607
Loreta Korf <kofekorf@gmail.com>
2022-11-10 20:41:24 +0200

    JS beginning

---
+++ JS/scripts.js
@@ -0,0 +1,47 @@
+// let svoris = prompt('Koks jusu svoris? (kg)');
+// let svorisKg = Number(svoris);
+
+// let height = prompt('Koks jusu ugis? (cm)');
+// let heightCm = Number(height);
+// let heightCmKv = heightCm**2;
+
+// const daugiklis = 10_000;
+
+// let bmi = svorisKg / heightCmKv * daugiklis;
+
+// console.log(bmi);
+
+// alert(`Jusu bmi yra ${bmi}`)
+
+let svoris = prompt('Koks jusu svoris? (kg)');
+svoris = Number(svoris);
+const height = Number(prompt('Koks jusu ugis? (cm)'));
+const daugiklis = 10_000;
+const bmi = (svoris / height ** 2) * daugiklis;
+alert(`Jusu BMI: ${bmi}`);
+
+
+
+
+
+
+// let x = 0.01;
+// let tipas = typeof x;
+// console.log(typeof tipas);
+// console.log(typeof 0.01);
+
+//string
+// let typeText = `variable "type" yra "${type}"`;
+//boolean
+//true
+// let x = 123;
+// let tipas = typeof ((1/2) * 3 + "asd") === "number"; //"number"
+// console.log((1/2) * 3 + "asd");
+// console.log(typeof ((1/2) * 3 + "asd"));
+
+// let isEqual = 123 == "123"; //true
+
+// let isEven = 123 == "123";
+// let string123 = String(123);
+// console.log(typeof string123);
+
